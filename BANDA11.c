905fca4ab15bdaa64fbc6f5c86d607c4222af8e8
jacoby <jacoby@itba.edu.ar>
2013-09-24 17:01:52 -0300

    Primera version andando agrege verificacion de paridad

--- BANDA11.c
+++ BANDA11.c
@@ -26,11 +26,22 @@
 
 
 #define START_SENTINEL  0xB  /* P 1101  P=0 (odd parity) */
+#define END_SENTINEL  0x1F  /* P 1111  P=1 (odd parity) */
+#define FIELD_SEPARATOR  0x0D  /* P 1101  P=0 (odd parity) */
 
 
 #define  ABA_MASK 0xF8
 
-#define DEBUG FALSE
+#define DEBUG TRUE
+
+
+#define WAITING_FOR_SS  0
+
+#define WAITING_FOR_DATA  1
+                              
+#define MAXCAP  300
+
+#define OK 1 
 
 
 
@@ -58,14 +69,15 @@ enable	equ	7	      |	 enable       ->  Amarillo   |
 */
 
 
-BYTE a[500];
-
+BYTE capture [MAXCAP];
+BYTE data_buff[100];                
+unsigned int pnt=0;
 
 BYTE read_ban(void);
 
 void assemble(unsigned char bit);
-
-
+void slow(void);
+BYTE chk_odd_parity(BYTE val);
 
 void main(void) {
   
@@ -81,7 +93,7 @@ BYTE read_ban(void)
 
 {
   
-
+  static tmpbit;
   
   int i=0,j;
 
@@ -98,13 +110,16 @@ BYTE read_ban(void)
 		          /*     assemble(PORTBAN & PIN_DATA); */
 				      /*   for(j=3;j>0;j--); / settle time */
 				      
+				      
+				    tmpbit=(PORTBAN & PIN_DATA)^PIN_DATA;  /* Isolate bit and invert */
+				      
 		        #if DEBUG == TRUE
 		        
-		        a[i++]=(PORTBAN & PIN_DATA)^PIN_DATA;  /* Isolate bit and invert */
+		        capture[i++]=tmpbit;  
 		
 		        #endif
 		        
-		        assemble((PORTBAN & PIN_DATA)^PIN_DATA);
+		        /* assemble(tmpbit); */          /* only for fast processors */ 
 		        
 		        
 		        while ((PORTBAN & PIN_CLOCK)==CLK_LOW); /* Wait for clock rising edge */
@@ -114,12 +129,14 @@ BYTE read_ban(void)
 		    
 	    }		    
 		    
-		    
+		    slow();
 	
   } 
   
   else 
   {
+      
+      
     
     return(1);
   
@@ -130,90 +147,163 @@ return(0);
 }
 
 
+void slow(void)
+
+{
+
+int i;
+
+    for(i=0;i<=MAXCAP;i++)
+    
+    {
+        assemble( capture[i] );
+      
+    }
+    
+    
+    
+    
+
+
+}
+
+
+
 
 
 void assemble(unsigned char bit)
 
 {
 		
-static BYTE rx_data=0;
+static BYTE rx_data=0;      
+static BYTE state= WAITING_FOR_SS;
+static count=5;
+
+ 
 
 
-   
-mal    	rx_data>>=bit;
-
-
-
-      if( (rx_data & ABA_MASK) == START_SENTINEL) {
+        rx_data>>=1;
         
-        printf("SS found");
+        if(bit)
+          rx_data|=0x80;
+        else
+          rx_data&=0x7F;      
+
+
+    
+    switch (state)  {
+    
+   
+   
+  case WAITING_FOR_SS:
+    
+      if( (rx_data & ABA_MASK) == (START_SENTINEL << 3)) {
+        
+        printf("SS found \n");
+        state=WAITING_FOR_DATA;
+        count=5;
+        pnt=0;
         
       }
         
-      
+       break;              
 
 
+   case WAITING_FOR_DATA:
+   
+          putchar((rx_data&0x80)?0x31:0x30);
+          
+         
+          if((--count)==0) { /*if a character was assembled on rx_data store it otherwise continue the assembly process*/
+          
+                      
+              printf("Storing data %d\n",count);
+              
+             
+               /*Check for parity*/
+              
+               if(chk_odd_parity(rx_data & ABA_MASK)!=OK)
+               
+                  printf("Parity Error at data %d \n",pnt);
+                
+                              
+                           
+
+                  /*Store assembled data by :
+                  
+                  1- Isolating P b3 b2 b1 b0 (Most significant part of byte rx_data see ABA MASK definiton ) 
+                  2- Shift down until least significand bit of rx_data is reached  
+                  3- Set  P=0 
+                  4- Add ASCII bias */                           
+              
+                  data_buff[pnt++]= ((((rx_data&=ABA_MASK)>>3)&0x0F) |0x30);      
+                  count=5;  /*Start a new character*/
+           
+       
+       
+              /*Check if end of Track*/
+          
+              if( (rx_data & ABA_MASK) == (END_SENTINEL << 3)) {        /*End Sentinel ??*/
+           
+                  state=WAITING_FOR_SS;                /*Restart*/
+               
+                  data_buff[--pnt]=0;                  /*Put null terminator on data string */
+              
+                  printf("Data Recieved %s \n",data_buff);
+              
+                  pnt=0;
+              
+              
+                  printf("END Seninel \n");
+              
+             }    
+              
+              
+          }
+
+         break;
+         
+     default:
+   
+           printf("Something wrong !!!! \n");
+          
+          break;
+         
+     }
 
 }
 
-/*	if (bit_count > (0x80 + 7))   // dejo pasar los primeros
-	
-			return;
-	
-*/	
-	
-	
-	
-	
-		
-	
-/*	
-	switch (state)
 
+/* ============================================================================
+ * This funcion asumes that data is stored in the MS part of byte val
+ *
+ * i.e   p b3 b2 b1 b0 0 0 0 
+ *
+ *
+ * It returns 1 (OK) if parity is odd 0 otherwise
+ *
+ * Note: even parity can be checked by Strarting pty=1 instead of 0 
+ ==============================================================================
+ */
 
-	case (WAIT_FOR_ONE):
+BYTE chk_odd_parity(BYTE val) 
+{
+  
+  BYTE walking_one=0x80;
+  BYTE pty=0;
+  int i; 
+  
 
-	
-		if(bit&0x01)
-		{
-			bit_count=5;	
-			state=START_READING;		
-	
-		}
-		else
-	 
-		return;
-	
-	
-	break;
+    for (i=1;i<=5;i++)    /*Count how many ones are in val*/
+    {
+  
+      if(val&walking_one)   /*if a one is present inc pty counter*/
+         pty++;
 
-	case(START_READING):
-	
-		if(bit_count)
-		{
-			rx_data>>=bit;
-			bit_count--;
-		}	
-		else 
-		{
-			if(rx_data==SS)
-			{
-				*p2buffer++=rx_data&DATA_MASK;	
-				bit_count=5;
-			{
-			else 
-			{
+      walking_one>>=1;      /*¨Sift one a place to right */
 
-							
+    }
 
-		}
-	
+    return(pty&0x01);  /* Return counter lsb bit (if parrity is odd a 1 (OK) will be returned */
 
-	break;		
-
-		
-	default:
-	break;
-			
-*/
-		
+}
